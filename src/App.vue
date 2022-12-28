b59a7d0010e4ae49649cb4b07f37bd343ad0f8f3
themegazord <contato.wanjalagus@outlook.com.br>
2022-12-28 19:00:48 -0400

    (dev) first commit

---
+++ src/App.vue
@@ -0,0 +1,81 @@
+<template>
+  <div id="app">
+    <navBar />
+    <div id="agenda-consulta-homepage">
+      <div class="pets-agenda-consulta-homepage">
+        <img class="dog-golden" src="@/assets/golden.png" alt="Imagem de um cachorro da raça Golden Retriever">
+        <img class="dog-maltes" src="@/assets/maltes.png" alt="Imagem de um cachorro da raça Maltês">
+        <img class="cat-persian" src="@/assets/gato_persa.png" alt="Imagem de um gato persa ">
+      </div>
+      <div class="info-agenda-consulta-homepage">
+        <h1 class="title-info-agenda-consulta-homepage">
+          Bem vindo ao CadUni Veterinário
+        </h1>
+        <p class="description-info-agenda-consulta-homepage">Cadastre seu pet e tenha todas as informações sempre que precisar na palma da mão!</p>
+        <button type="button" class="btn-info-agenda-consulta-homepage">Cadastre-se</button>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+
+import navBar from "@/components/Navbar";
+
+export default {
+    name: "App",
+    components: {
+        navBar,
+    }
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Baloo 2', sans-serif;
+}
+#app {
+  width: 100vw;
+  height: 100vh;
+}
+
+#agenda-consulta-homepage {
+  background-color: #6ADBD9;
+  height: calc(100vh - 110px);
+  width: 100%;
+  position: relative;
+  display: grid;
+  grid-template-columns: 50% 50%;
+}
+
+.dog-golden {
+  position: absolute;
+  width: 250px;
+  height: 350px;
+  left: 96px;
+  top: 162px;
+  z-index: 1;
+}
+
+.dog-maltes{
+  position: absolute;
+  width: 240px;
+  height: 290px;
+  left: 250px;
+  top: 250px;
+  z-index: 3;
+}
+
+.cat-persian {
+  position: absolute;
+  width: 260px;
+  height: 270px;
+  left: 350px;
+  top: 250px;
+  z-index: 2;
+}
+
+
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,74 @@
+<template>
+  <nav id="nav">
+   <img src="../assets/logo_cadunivet.png" alt="Logo da CadUni Veterinárias" id="logo">
+    <ul class="navbar-links"> 
+        <!-- TODO passar os links por props -->
+        <li class="link">Agende sua consulta</li>
+        <li class="link">Sobre nós</li>
+        <li class="link">Depoimentos</li>
+        <li class="link">Contato</li>
+        <li class="link">
+            <span class="btn-login">Login <img class="dog-login" src="@/assets/cao.png" alt="Cachorrinho do Login"></span>
+        </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+    name: "nav-bar",
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Baloo+2:wght@400;500;700&display=swap');
+#nav {
+    position: relative;
+    left: 0;
+    top: 0;
+    height: 110px;
+    background-color: #CAFCFB;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+
+#logo {
+    width: 158px;
+    height: 130px;
+    cursor: pointer;
+}
+
+.navbar-links {
+    display: flex;
+    gap: 30px;
+    margin-right: 50px;
+    align-items: center;
+    justify-content: center;
+}
+
+.link {
+    color: #09BBB5;
+    list-style: none;
+    cursor: pointer;
+    font-size: 18px;
+}
+
+.dog-login {
+    width: 32px;
+    height: 32px;
+}
+
+.btn-login {
+    width: 140px;
+    height: 50px;
+    background-color: #09BBB5;
+    color: #FFFFFF;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 5px;
+    border-radius: 100px;
+}
+</style>
