6630e01f57b85a61e77788797e7cc87a91184706
themegazord <contato.wanjalagus@outlook.com.br>
2023-01-02 14:26:53 -0400

    (dev) retirado as views e rotas de distinção de cadastro

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,5 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import cadastroUsuarios from "@/views/cadastro-usuarios/CadastroUsuarios.vue";
-import cadastroEmpresa from "@/views/cadastro-empresa/CadastroEmpresa.vue";
 import cadastro from "@/views/cadastro/Cadastro.vue";
 import homePage from "@/views/home/Home.vue";
 
@@ -18,16 +16,7 @@ const routes = [
         name: "cadastro-geral",
         component: cadastro
     },
-    {
-        path: "/cadastro/usuarios",
-        name: "cadastro-usuarios",
-        component: cadastroUsuarios,
-    },
-    {
-        path: "/cadastro/empresa",
-        name: "cadastro-empresa",
-        component: cadastroEmpresa
-    },
+    
     
 ];
 
